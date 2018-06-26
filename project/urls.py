2d3f0337ac406e6fc9d1ea0912a60ad04ce8ebd8
shori <shori@digitalgeko.com>
2018-06-26 08:23:55 -0600

    use provided template project

--- project/urls.py
+++ project/urls.py
@@ -5,6 +5,10 @@ from django.contrib import admin
 from welcome.views import index, health
 
 urlpatterns = [
+    # Examples:
+    # url(r'^$', 'project.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
     url(r'^$', index),
     url(r'^health$', health),
     url(r'^admin/', include(admin.site.urls)),
